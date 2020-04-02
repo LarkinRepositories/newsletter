5e5b478327141acc68f7e9a6045b15b25a9b4ad8
larkinds <larkindsx@gmail.com>
2020-04-02 15:43:03 +0700

    News repository added. Repository layer completed

---
+++ src/main/java/com/larkinds/sibers/repository/NewsRepository.java
@@ -0,0 +1,33 @@
+package com.larkinds.sibers.repository;
+
+import com.larkinds.sibers.model.News;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NewsRepository extends JpaRepository<News, Long> {
+    /**
+     * Returns page with limited news count
+     * @param pageable news count limiter
+     * @return page of news
+     */
+    Page<News> findAll(Pageable pageable);
+
+    /**
+     * Returns page of news containing specific text
+     * @param searchText text to search
+     * @param  titleText title text to search
+     * @param pageable limiter
+     * @return page of news containing specific text
+     */
+    Page<News> findAllByTextContainingOrTitleContaining(String searchText, String titleText, Pageable pageable);
+
+    /**
+     * Checks if news record with a specific title exists in database
+     * @param title title of the news
+     * @return exists ? true : false
+     */
+    boolean existsByTitle(String title);
+}
